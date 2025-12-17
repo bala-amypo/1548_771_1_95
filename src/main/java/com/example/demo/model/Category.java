66c7b5ddfbac9b91a025c83ba5d42bb920baddd8
bala-amypo <bala@amypo.in>
2025-12-17 08:29:20 +0000

    User commit

--- src/main/java/com/example/demo/model/Category.java
+++ src/main/java/com/example/demo/model/Category.java
@@ -1,10 +1,16 @@
-package com.example.demo.model;
+package com.example.demo1.model;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
 @Entity
 public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
     private Long id;
     @Column(unique=true)
     private String name;
--- src/main/java/com/example/demo/model/TransactionLog.java
+++ src/main/java/com/example/demo/model/TransactionLog.java
@@ -1,74 +1,74 @@
-package com.example.demo.model;
+// package com.example.demo.model;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+// import jakarta.persistence.Column;
+// import jakarta.persistence.Entity;
+// import jakarta.persistence.GeneratedValue;
+// import jakarta.persistence.GenerationType;
+// import jakarta.persistence.Id;
+// import jakarta.persistence.Table;
 
 
-@Entity
-@Table(name = "users")
-public class User {
+// @Entity
+// @Table(name = "users")
+// public class User {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    // private User name;
-    // private Category email;
-    private String description;
-    private String role;
+//     @Id
+//     @GeneratedValue(strategy = GenerationType.IDENTITY)
+//     private Long id;
+//     // private User name;
+//     // private Category email;
+//     private String description;
+//     private String role;
 
-    public TransactionLog() {
+//     public TransactionLog() {
 
-    }
+//     }
 
-    public TransactionLog(String name, String email, String password, String role) {
-        this.name = name;
-        this.email = email;
-        this.password = password;
-        this.role = role;
-    }
+//     public TransactionLog(String name, String email, String password, String role) {
+//         this.name = name;
+//         this.email = email;
+//         this.password = password;
+//         this.role = role;
+//     }
 
-    public Long getId() {
-        return id;
-    }
+//     public Long getId() {
+//         return id;
+//     }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+//     public void setId(Long id) {
+//         this.id = id;
+//     }
 
-    public String getName() {
-        return name;
-    }
+//     public String getName() {
+//         return name;
+//     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
+//     public void setName(String name) {
+//         this.name = name;
+//     }
 
-    public String getEmail() {
-        return email;
-    }
+//     public String getEmail() {
+//         return email;
+//     }
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
+//     public void setEmail(String email) {
+//         this.email = email;
+//     }
 
-    public String getPassword() {
-        return password;
-    }
+//     public String getPassword() {
+//         return password;
+//     }
 
-    public void setPassword(String password) {
-        this.password = password;
-    }
+//     public void setPassword(String password) {
+//         this.password = password;
+//     }
 
-    public String getRole() {
-        return role;
-    }
+//     public String getRole() {
+//         return role;
+//     }
 
-    public void setRole(String role) {
-        this.role = role;
-    }
-}
+//     public void setRole(String role) {
+//         this.role = role;
+//     }
+// }
 
