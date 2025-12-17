8bc94c7423e4b4d6ec214ac8aaabef4cb06c4c36
bala-amypo <bala@amypo.in>
2025-12-17 10:24:20 +0000

    User commit

--- src/main/java/com/example/demo/model/TransactionLog.java
+++ src/main/java/com/example/demo/model/TransactionLog.java
@@ -1,74 +1,92 @@
-// package com.example.demo.model;
 
-// import jakarta.persistence.Column;
-// import jakarta.persistence.Entity;
-// import jakarta.persistence.GeneratedValue;
-// import jakarta.persistence.GenerationType;
-// import jakarta.persistence.Id;
-// import jakarta.persistence.Table;
+package com.example.demo1.model;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.Past;
 
 
-// @Entity
-// @Table(name = "users")
-// public class User {
+@Entity
+@Table(name = "users")
+public class TransactionLog {
 
-//     @Id
-//     @GeneratedValue(strategy = GenerationType.IDENTITY)
-//     private Long id;
-//     // private User name;
-//     // private Category email;
-//     private String description;
-//     private String role;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private User user;
+    private Category category;
+    @Min(value = 1)
+    private Double amount;
+    private String description;
+    @Past
+    private LocalDate transactionDate;
 
-//     public TransactionLog() {
+    public TransactionLog() {
 
-//     }
+    }
 
-//     public TransactionLog(String name, String email, String password, String role) {
-//         this.name = name;
-//         this.email = email;
-//         this.password = password;
-//         this.role = role;
-//     }
+    public TransactionLog(User user, Category category, Double amount, String description, LocalDate transactionDate) {
+        this.user = user;
+        this.category = category;
+        this.amount = amount;
+        this.description = description;
+        this.transactionDate = transactionDate;
+    }
 
-//     public Long getId() {
-//         return id;
-//     }
+    public Long getId() {
+        return id;
+    }
 
-//     public void setId(Long id) {
-//         this.id = id;
-//     }
+    public User getUser() {
+        return user;
+    }
 
-//     public String getName() {
-//         return name;
-//     }
+    public Category getCategory() {
+        return category;
+    }
 
-//     public void setName(String name) {
-//         this.name = name;
-//     }
+    public Double getAmount() {
+        return amount;
+    }
 
-//     public String getEmail() {
-//         return email;
-//     }
+    public String getDescription() {
+        return description;
+    }
 
-//     public void setEmail(String email) {
-//         this.email = email;
-//     }
+    public LocalDate getTransactionDate() {
+        return transactionDate;
+    }
 
-//     public String getPassword() {
-//         return password;
-//     }
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-//     public void setPassword(String password) {
-//         this.password = password;
-//     }
+    public void setUser(User user) {
+        this.user = user;
+    }
 
-//     public String getRole() {
-//         return role;
-//     }
+    public void setCategory(Category category) {
+        this.category = category;
+    }
 
-//     public void setRole(String role) {
-//         this.role = role;
-//     }
-// }
+    public void setAmount(Double amount) {
+        this.amount = amount;
+    }
 
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setTransactionDate(LocalDate transactionDate) {
+        this.transactionDate = transactionDate;
+    }
+
+    
+
+}
