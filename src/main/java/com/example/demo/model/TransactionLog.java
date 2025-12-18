ba938537d1765ed191731ee5347a493cc14f5217
bala-amypo <bala@amypo.in>
2025-12-18 05:16:19 +0000

    User commit

--- src/main/java/com/example/demo/model/TransactionLog.java
+++ src/main/java/com/example/demo/model/TransactionLog.java
@@ -1,92 +1,92 @@
 
-package com.example.demo1.model;
+// package com.example.demo1.model;
 
-import java.time.LocalDate;
+// import java.time.LocalDate;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.Past;
+// import jakarta.persistence.Entity;
+// import jakarta.persistence.GeneratedValue;
+// import jakarta.persistence.GenerationType;
+// import jakarta.persistence.Id;
+// import jakarta.persistence.Table;
+// import jakarta.validation.constraints.Min;
+// import jakarta.validation.constraints.Past;
 
 
-@Entity
-@Table(name = "users")
-public class TransactionLog {
+// @Entity
+// @Table(name = "users")
+// public class TransactionLog {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    private User user;
-    private Category category;
-    @Min(value = 1)
-    private Double amount;
-    private String description;
-    @Past
-    private LocalDate transactionDate;
+//     @Id
+//     @GeneratedValue(strategy = GenerationType.IDENTITY)
+//     private Long id;
+//     private User user;
+//     private Category category;
+//     @Min(value = 1)
+//     private Double amount;
+//     private String description;
+//     @Past
+//     private LocalDate transactionDate;
 
-    public TransactionLog() {
+//     public TransactionLog() {
 
-    }
+//     }
 
-    public TransactionLog(User user, Category category, Double amount, String description, LocalDate transactionDate) {
-        this.user = user;
-        this.category = category;
-        this.amount = amount;
-        this.description = description;
-        this.transactionDate = transactionDate;
-    }
+//     public TransactionLog(User user, Category category, Double amount, String description, LocalDate transactionDate) {
+//         this.user = user;
+//         this.category = category;
+//         this.amount = amount;
+//         this.description = description;
+//         this.transactionDate = transactionDate;
+//     }
 
-    public Long getId() {
-        return id;
-    }
+//     public Long getId() {
+//         return id;
+//     }
 
-    public User getUser() {
-        return user;
-    }
+//     public User getUser() {
+//         return user;
+//     }
 
-    public Category getCategory() {
-        return category;
-    }
+//     public Category getCategory() {
+//         return category;
+//     }
 
-    public Double getAmount() {
-        return amount;
-    }
+//     public Double getAmount() {
+//         return amount;
+//     }
 
-    public String getDescription() {
-        return description;
-    }
+//     public String getDescription() {
+//         return description;
+//     }
 
-    public LocalDate getTransactionDate() {
-        return transactionDate;
-    }
+//     public LocalDate getTransactionDate() {
+//         return transactionDate;
+//     }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+//     public void setId(Long id) {
+//         this.id = id;
+//     }
 
-    public void setUser(User user) {
-        this.user = user;
-    }
+//     public void setUser(User user) {
+//         this.user = user;
+//     }
 
-    public void setCategory(Category category) {
-        this.category = category;
-    }
+//     public void setCategory(Category category) {
+//         this.category = category;
+//     }
 
-    public void setAmount(Double amount) {
-        this.amount = amount;
-    }
+//     public void setAmount(Double amount) {
+//         this.amount = amount;
+//     }
 
-    public void setDescription(String description) {
-        this.description = description;
-    }
+//     public void setDescription(String description) {
+//         this.description = description;
+//     }
 
-    public void setTransactionDate(LocalDate transactionDate) {
-        this.transactionDate = transactionDate;
-    }
+//     public void setTransactionDate(LocalDate transactionDate) {
+//         this.transactionDate = transactionDate;
+//     }
 
     
 
-}
+// }
