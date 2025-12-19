43247d56d12518367094167deb557fd172e49b5d
bala-amypo <bala@amypo.in>
2025-12-19 05:25:52 +0000

    User commit

--- src/main/java/com/example/demo/controller/AuthController.java
+++ src/main/java/com/example/demo/controller/AuthController.java
@@ -1,5 +1,44 @@
 
-package com.example.demo.controller;
+// package com.example.demo.controller;
+
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.web.bind.annotation.PostMapping;
+// import org.springframework.web.bind.annotation.RequestBody;
+// import org.springframework.web.bind.annotation.RequestMapping;
+// import org.springframework.web.bind.annotation.RestController;
+
+// import com.example.demo.model.User;
+// import com.example.demo.service.UserService;
+
+// @RestController
+// @RequestMapping("/auth")
+// public class AuthController {
+
+//     @Autowired
+//     private UserService userService;
+
+//     @PostMapping("/register")
+//     public User register(@RequestBody User user) {
+//         return userService.register(user);
+//     }
+
+//     @PostMapping("/login")
+//     public String login(@RequestBody User user) {
+//         User dbUser = userService.findByEmail(user.getEmail());
+
+//         if (dbUser == null) {
+//             return "Invalid email or password";
+//         }
+
+//         if (dbUser.getPassword().equals(user.getPassword())) {
+//             return "Login successful";
+//         }
+
+//         return "Invalid email or password";
+//     }
+// }
+
+package com.example.demo3.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -7,8 +46,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.example.demo.model.User;
-import com.example.demo.service.UserService;
+import com.example.demo3.model.User;
+import com.example.demo3.service.UserService;
 
 @RestController
 @RequestMapping("/auth")
@@ -21,7 +60,6 @@ public class AuthController {
     public User register(@RequestBody User user) {
         return userService.register(user);
     }
-
     @PostMapping("/login")
     public String login(@RequestBody User user) {
         User dbUser = userService.findByEmail(user.getEmail());
@@ -29,12 +67,12 @@ public class AuthController {
         if (dbUser == null) {
             return "Invalid email or password";
         }
-
         if (dbUser.getPassword().equals(user.getPassword())) {
-            return "Login successful";
+            return "Login successful! Welcome " + dbUser.getName() + " (Role: " + dbUser.getRole() + ")";
         }
 
         return "Invalid email or password";
     }
 }
+
 
