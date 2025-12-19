8f8fd56b77e61e8698ebb6f81d0075018ac26daf
bala-amypo <bala@amypo.in>
2025-12-19 09:15:41 +0000

    User commit

--- src/main/java/com/example/demo/service/Impl/CategoryServiceImpl.java
+++ src/main/java/com/example/demo/service/Impl/CategoryServiceImpl.java
@@ -30,48 +30,48 @@
 // }
 
 
-package com.example.demo.service.impl;
+// package com.example.demo.service.impl;
 
-import java.util.List;
+// import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.stereotype.Service;
 
-import com.example.demo.model.Category;
-import com.example.demo.repository.CategoryRepository;
-import com.example.demo.service.CategoryService;
+// import com.example.demo.model.Category;
+// import com.example.demo.repository.CategoryRepository;
+// import com.example.demo.service.CategoryService;
 
 
-@Service
-public class CategoryServiceImpl implements CategoryService {
+// @Service
+// public class CategoryServiceImpl implements CategoryService {
 
-    @Autowired
-    private CategoryRepository categoryRepository;
+//     @Autowired
+//     private CategoryRepository categoryRepository;
 
-    @Override
-    public Category addCategory(Category category) {
-        if (categoryRepository.existsByName(category.getName())) {
-            throw new RuntimeException("Category already exists: " + category.getName());
-        }
+//     @Override
+//     public Category addCategory(Category category) {
+//         if (categoryRepository.existsByName(category.getName())) {
+//             throw new RuntimeException("Category already exists: " + category.getName());
+//         }
 
-        final String type = category.getType();
+//         final String type = category.getType();
         
-        if (Category.TYPE_EXPENSE.equalsIgnoreCase(type)) {
-            category.setType(Category.TYPE_EXPENSE);
-        } else if (Category.TYPE_INCOME.equalsIgnoreCase(type)) {
-            category.setType(Category.TYPE_INCOME);
-        } else {
-            category.setType(Category.TYPE_INCOME);
-        }
+//         if (Category.TYPE_EXPENSE.equalsIgnoreCase(type)) {
+//             category.setType(Category.TYPE_EXPENSE);
+//         } else if (Category.TYPE_INCOME.equalsIgnoreCase(type)) {
+//             category.setType(Category.TYPE_INCOME);
+//         } else {
+//             category.setType(Category.TYPE_INCOME);
+//         }
 
-        return categoryRepository.save(category);
-    }
+//         return categoryRepository.save(category);
+//     }
 
-    @Override
-    public List<Category> getAllCategories() {
-        return categoryRepository.findAll();
-    }
-}
+//     @Override
+//     public List<Category> getAllCategories() {
+//         return categoryRepository.findAll();
+//     }
+// }
 
 
 
