61d460d15f28615be95eab5e72e9fc7fcf637602
bala-amypo <bala@amypo.in>
2025-12-19 04:53:42 +0000

    User commit

--- src/main/java/com/example/demo/model/User.java
+++ src/main/java/com/example/demo/model/User.java
@@ -13,72 +13,73 @@ import jakarta.validation.constraints.Email;
 public class User {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+            private Long id;
 
-    @Column(nullable = false)
-    private String name;
+                @Column(nullable = false)
+                    private String name;
 
-    @Email
-    @Column(unique = true, nullable = false)
-    private String email;
+                        @Email
+                            @Column(unique = true, nullable = false)
+                                private String email;
 
-    @Column(nullable = false)
-    private String password;
+                                    @Column(nullable = false)
+                                        private String password;
 
-    @Column(nullable = false)
-    private String role;
+                                            @Column(nullable = false)
+                                                private String role;
 
-    public User() {
-    }
+                                                    public User() {
+                                                        }
 
-    public User(String name, String email, String password, String role) {
-        this.name = name;
-        this.email = email;
-        this.password = password;
-        this.role = role;
-    }
+                                                            public User(String name, String email, String password, String role) {
+                                                                    this.name = name;
+                                                                            this.email = email;
+                                                                                    this.password = password;
+                                                                                            this.role = role;
+                                                                                                }
 
-    public Long getId() {
-        return id;
-    }
+                                                                                                    public Long getId() {
+                                                                                                            return id;
+                                                                                                                }
 
-    public String getName() {
-        return name;
-    }
+                                                                                                                    public String getName() {
+                                                                                                                            return name;
+                                                                                                                                }
 
-    public String getEmail() {
-        return email;
-    }
+                                                                                                                                    public String getEmail() {
+                                                                                                                                            return email;
+                                                                                                                                                }
 
-    public String getPassword() {
-        return password;
-    }
+                                                                                                                                                    public String getPassword() {
+                                                                                                                                                            return password;
+                                                                                                                                                                }
 
-    public String getRole() {
-        return role;
-    }
+                                                                                                                                                                    public String getRole() {
+                                                                                                                                                                            return role;
+                                                                                                                                                                                }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+                                                                                                                                                                                    public void setId(Long id) {
+                                                                                                                                                                                            this.id = id;
+                                                                                                                                                                                                }
 
-    public void setName(String name) {
-        this.name = name;
-    }
+                                                                                                                                                                                                    public void setName(String name) {
+                                                                                                                                                                                                            this.name = name;
+                                                                                                                                                                                                                }
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
+                                                                                                                                                                                                                    public void setEmail(String email) {
+                                                                                                                                                                                                                            this.email = email;
+                                                                                                                                                                                                                                }
 
-    public void setPassword(String password) {
-        this.password = password;
-    }
+                                                                                                                                                                                                                                    public void setPassword(String password) {
+                                                                                                                                                                                                                                            this.password = password;
+                                                                                                                                                                                                                                                }
 
-    public void setRole(String role) {
-        this.role = role;
-    }
+                                                                                                                                                                                                                                                    public void setRole(String role) {
+                                                                                                                                                                                                                                                            this.role = role;
+                                                                                                                                                                                                                                                                }
 
-   
-}
+                                                                                                                                                                                                                                                                   
+                                                                                                                                                                                                                                                                   }
 
+                                                                                                                                                                                                                                                                   
