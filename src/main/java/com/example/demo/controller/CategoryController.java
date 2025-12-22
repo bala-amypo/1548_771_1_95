9373438188be3eb563e1e910aeaf83c67149ca0e
bala-amypo <bala@amypo.in>
2025-12-22 03:14:15 +0000

    User commit

--- src/main/java/com/example/demo/controller/CategoryController.java
+++ src/main/java/com/example/demo/controller/CategoryController.java
@@ -65,40 +65,6 @@
 
 
 
-// package com.example.demo.controller;
-
-// import java.util.List;
-
-// import org.springframework.web.bind.annotation.GetMapping;
-// import org.springframework.web.bind.annotation.PostMapping;
-// import org.springframework.web.bind.annotation.RequestBody;
-// import org.springframework.web.bind.annotation.RequestMapping;
-// import org.springframework.web.bind.annotation.RestController;
-
-// import com.example.demo.model.Category;
-// import com.example.demo.service.CategoryService;
-
-
-// @RestController
-// @RequestMapping("/categories")
-// public class CategoryController {
-
-//     private final CategoryService categoryService;
-
-//     public CategoryController(CategoryService categoryService) {
-//         this.categoryService = categoryService;
-//     }
-
-//     @PostMapping
-//     public Category create(@RequestBody Category category) {
-//         return categoryService.addCategory(category);
-//     }
-
-//     @GetMapping
-//     public List<Category> getAll() {
-//         return categoryService.getAllCategories();
-//     }
-// }
 package com.example.demo.controller;
 
 import java.util.List;
@@ -109,26 +75,60 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.example.demo4.model.Category;
-import com.example.demo4.service.CategoryService;
+import com.example.demo.model.Category;
+import com.example.demo.service.CategoryService;
+
 
 @RestController
 @RequestMapping("/categories")
 public class CategoryController {
 
-    private final CategoryService service;
+    private final CategoryService categoryService;
 
-    public CategoryController(CategoryService service) {
-        this.service = service;
+    public CategoryController(CategoryService categoryService) {
+        this.categoryService = categoryService;
     }
 
     @PostMapping
     public Category create(@RequestBody Category category) {
-        return service.addCategory(category); 
+        return categoryService.addCategory(category);
     }
 
     @GetMapping
-    public List<Category> all() {
-        return service.getAllCategories();     
+    public List<Category> getAll() {
+        return categoryService.getAllCategories();
     }
 }
+// package com.example.demo.controller;
+
+// import java.util.List;
+
+// import org.springframework.web.bind.annotation.GetMapping;
+// import org.springframework.web.bind.annotation.PostMapping;
+// import org.springframework.web.bind.annotation.RequestBody;
+// import org.springframework.web.bind.annotation.RequestMapping;
+// import org.springframework.web.bind.annotation.RestController;
+
+// import com.example.demo4.model.Category;
+// import com.example.demo4.service.CategoryService;
+
+// @RestController
+// @RequestMapping("/categories")
+// public class CategoryController {
+
+//     private final CategoryService service;
+
+//     public CategoryController(CategoryService service) {
+//         this.service = service;
+//     }
+
+//     @PostMapping
+//     public Category create(@RequestBody Category category) {
+//         return service.addCategory(category); 
+//     }
+
+//     @GetMapping
+//     public List<Category> all() {
+//         return service.getAllCategories();     
+//     }
+// }
