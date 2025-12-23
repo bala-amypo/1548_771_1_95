542e8fafd4f1311be2b7e5a07544a907e99a1227
bala-amypo <bala@amypo.in>
2025-12-23 18:01:51 +0000

    User commit

--- src/main/java/com/example/demo/controller/CategoryController.java
+++ src/main/java/com/example/demo/controller/CategoryController.java
@@ -1,18 +1,46 @@
 
 
+// // package com.example.demo.controller;
+
+// // import java.util.List;
+
+// // import org.springframework.web.bind.annotation.GetMapping;
+// // import org.springframework.web.bind.annotation.PostMapping;
+// // import org.springframework.web.bind.annotation.RequestBody;
+// // import org.springframework.web.bind.annotation.RequestMapping;
+// // import org.springframework.web.bind.annotation.RestController;
+
+// // import com.example.demo.model.Category;
+// // import com.example.demo.service.CategoryService;
+
+
+// // @RestController
+// // @RequestMapping("/categories")
+// // public class CategoryController {
+
+// //     private final CategoryService categoryService;
+
+// //     public CategoryController(CategoryService categoryService) {
+// //         this.categoryService = categoryService;
+// //     }
+
+// //     @PostMapping
+// //     public Category create(@RequestBody Category category) {
+// //         return categoryService.addCategory(category);
+// //     }
+
+// //     @GetMapping
+// //     public List<Category> getAll() {
+// //         return categoryService.getAllCategories();
+// //     }
+// // }
 // package com.example.demo.controller;
-
-// import java.util.List;
-
-// import org.springframework.web.bind.annotation.GetMapping;
-// import org.springframework.web.bind.annotation.PostMapping;
-// import org.springframework.web.bind.annotation.RequestBody;
-// import org.springframework.web.bind.annotation.RequestMapping;
-// import org.springframework.web.bind.annotation.RestController;
 
 // import com.example.demo.model.Category;
 // import com.example.demo.service.CategoryService;
+// import org.springframework.web.bind.annotation.*;
 
+// import java.util.List;
 
 // @RestController
 // @RequestMapping("/categories")
@@ -36,11 +64,12 @@
 // }
 package com.example.demo.controller;
 
-import com.example.demo.model.Category;
-import com.example.demo.service.CategoryService;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
+import com.example.demo.model.Category;
+import com.example.demo.service.CategoryService;
 
 @RestController
 @RequestMapping("/categories")
@@ -54,7 +83,7 @@ public class CategoryController {
 
     @PostMapping
     public Category create(@RequestBody Category category) {
-        return categoryService.addCategory(category);
+        return categoryService.createCategory(category);
     }
 
     @GetMapping
--- src/main/java/com/example/demo/service/Impl/CategoryServiceImpl.java
+++ src/main/java/com/example/demo/service/Impl/CategoryServiceImpl.java
@@ -42,32 +42,30 @@
 
 package com.example.demo.service.impl;
 
-import com.example.demo.exception.BadRequestException;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
 import com.example.demo.model.Category;
 import com.example.demo.repository.CategoryRepository;
 import com.example.demo.service.CategoryService;
 
-import java.util.List;
-
+@Service
 public class CategoryServiceImpl implements CategoryService {
 
-    private final CategoryRepository categoryRepository;
+    private final CategoryRepository repository;
 
-    public CategoryServiceImpl(CategoryRepository categoryRepository) {
-        this.categoryRepository = categoryRepository;
+    public CategoryServiceImpl(CategoryRepository repository) {
+        this.repository = repository;
     }
 
     @Override
-    public Category addCategory(Category category) {
-        if (categoryRepository.existsByName(category.getName())) {
-            throw new BadRequestException("Category already exists");
-        }
-        category.validateType();
-        return categoryRepository.save(category);
+    public Category createCategory(Category category) {
+        return repository.save(category);
     }
 
     @Override
     public List<Category> getAllCategories() {
-        return categoryRepository.findAll();
+        return repository.findAll();
     }
 }
