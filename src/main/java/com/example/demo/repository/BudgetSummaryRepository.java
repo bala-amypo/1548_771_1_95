bab6335ce605c781b0b5b3632e911663b973c076
bala-amypo <bala@amypo.in>
2025-12-23 16:55:51 +0000

    User commit

--- src/main/java/com/example/demo/repository/BudgetSummaryRepository.java
+++ src/main/java/com/example/demo/repository/BudgetSummaryRepository.java
@@ -1,15 +1,29 @@
 
+// package com.example.demo.repository;
+
+// import java.util.Optional;
+
+// import org.springframework.data.jpa.repository.JpaRepository;
+
+// import com.example.demo.model.BudgetPlan;
+// import com.example.demo.model.BudgetSummary;
+
+// public interface BudgetSummaryRepository
+//         extends JpaRepository<BudgetSummary, Long> {
+
+//     Optional<BudgetSummary> findByBudgetPlan(BudgetPlan plan);
+// }
+
+
 package com.example.demo.repository;
-
-import java.util.Optional;
-
-import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.demo.model.BudgetPlan;
 import com.example.demo.model.BudgetSummary;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface BudgetSummaryRepository
-        extends JpaRepository<BudgetSummary, Long> {
+import java.util.Optional;
 
-    Optional<BudgetSummary> findByBudgetPlan(BudgetPlan plan);
+public interface BudgetSummaryRepository extends JpaRepository<BudgetSummary, Long> {
+
+    Optional<BudgetSummary> findByBudgetPlan(BudgetPlan budgetPlan);
 }
