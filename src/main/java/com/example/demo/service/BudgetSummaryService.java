48698d6cda7ed7e6eb6f929dac7db6a45a9b8cf4
bala-amypo <bala@amypo.in>
2025-12-25 08:02:30 +0000

    User commit

--- src/main/java/com/example/demo/service/BudgetSummaryService.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.service;
-
-import com.example.demo.dto.BudgetSummaryResponse;
-
-public interface BudgetSummaryService {
-    BudgetSummaryResponse getSummary(Long userId, int month, int year);
-}
