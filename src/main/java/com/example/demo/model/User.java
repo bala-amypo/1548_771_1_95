a595c5dbb6473ab58dac2621d88185062a260f82
bala-amypo <bala@amypo.in>
2025-12-27 15:41:39 +0000

    User commit

--- src/main/java/com/example/demo/model/User.java
+++ src/main/java/com/example/demo/model/User.java
@@ -39,8 +39,6 @@ public class User {
         this.role = (role == null) ? ROLE_USER : role;
     }
 
-    // getters & setters
-
     public Long getId() { return id; }
     public void setId(Long id) { this.id = id; }
 
