de2852b7e923cad29faceafb66d00908be4f2bc8
bala-amypo <bala@amypo.in>
2025-12-27 01:54:44 +0000

    User commit

--- src/main/java/com/example/demo/dto/RegisterRequest.java
+++ src/main/java/com/example/demo/dto/RegisterRequest.java
@@ -1,56 +1,32 @@
-// package com.example.demo.dto;
-
-// import jakarta.validation.constraints.Email;
-// import jakarta.validation.constraints.NotBlank;
-
-// public class RegisterRequest {
-
-//     @NotBlank
-//     private String name;
-
-//     @Email
-//     @NotBlank
-//     private String email;
-
-//     @NotBlank
-//     private String password;
-
-//     private String role = "USER";
-
-//     public RegisterRequest() {}
-
-//     public RegisterRequest(String name, String email, String password) {
-//         this.name = name;
-//         this.email = email;
-//         this.password = password;
-//     }
-
-//     public String getName() {
-//         return name;
-//     }
-
-//     public String getEmail() {
-//         return email;
-//     }
-
-//     public String getPassword() {
-//         return password;
-//     }
-
-//     public String getRole() {
-//         return role;
-//     }
-// }
-
-
 package com.example.demo.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 
 public class RegisterRequest {
 
+    @NotBlank
+    private String name;
+
+    @Email
+    @NotBlank
     private String email;
+
+    @NotBlank
     private String password;
 
-    public RegisterRequest() {
+    private String role = "USER";
+
+    public RegisterRequest() {}
+
+    public RegisterRequest(String name, String email, String password) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
     }
 
     public String getEmail() {
@@ -61,11 +37,7 @@ public class RegisterRequest {
         return password;
     }
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
+    public String getRole() {
+        return role;
     }
 }
