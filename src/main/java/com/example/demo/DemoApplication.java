f2c31f4d22a7460d78b7e0c3df7f3d2ca0291def
bala-amypo <bala@amypo.in>
2025-12-28 12:57:29 +0000

    User commit

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -11,3 +11,17 @@
 // 	}
 
 // }
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+
+@SpringBootApplication
+@ServletComponentScan   // ✅ REQUIRED FOR @WebServlet
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+}
