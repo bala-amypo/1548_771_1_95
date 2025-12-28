5cdebdd75203ea20721c0427396277c4ca16ddde
bala-amypo <bala@amypo.in>
2025-12-28 12:42:29 +0000

    User commit

--- src/main/java/com/example/demo/dto/AuthResponse.java
+++ src/main/java/com/example/demo/dto/AuthResponse.java
@@ -1,56 +1,25 @@
 
 
-// // package com.example.demo.dto;
+package com.example.demo.dto;
 
-// // public class AuthResponse {
+public class AuthResponse {
 
-// //     private String message;
-// //     private String token;
+    private String message;
+    private String token;
 
-// //     public AuthResponse() {
-// //     }
+    public AuthResponse() {
+    }
 
-// //     public AuthResponse(String message, String token) {
-// //         this.message = message;
-// //         this.token = token;
-// //     }
+    public AuthResponse(String message, String token) {
+        this.message = message;
+        this.token = token;
+    }
 
-// //     public String getMessage() {
-// //         return message;
-// //     }
+    public String getMessage() {
+        return message;
+    }
 
-// //     public String getToken() {
-// //         return token;
-// //     }
-// // }
-// package com.example.demo.dto;
-
-// public class AuthResponse {
-
-//     private String message;
-//     private String token;
-
-//     public AuthResponse() {
-//     }
-
-//     public AuthResponse(String message, String token) {
-//         this.message = message;
-//         this.token = token;
-//     }
-
-//     public String getMessage() {
-//         return message;
-//     }
-
-//     public void setMessage(String message) {
-//         this.message = message;
-//     }
-
-//     public String getToken() {
-//         return token;
-//     }
-
-//     public void setToken(String token) {
-//         this.token = token;
-//     }
-// }
+    public String getToken() {
+        return token;
+    }
+}
--- src/main/java/com/example/demo/servlet/SimpleHelloServlet.java
+++ src/main/java/com/example/demo/servlet/SimpleHelloServlet.java
@@ -32,3 +32,4 @@ public class SimpleHelloServlet extends HttpServlet {
                                                                                                     public String getServletInfo() {
                                                                                                             return "SimpleHelloServlet Info";
                                                                                                     }
+}
